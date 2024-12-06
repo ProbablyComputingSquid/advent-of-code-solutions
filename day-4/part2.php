a441f8e8232defd9177db50f19333569723ebb8b
Jacob <66024281+ProbablyComputingSquid@users.noreply.github.com>
2024-12-06 21:02:12 +0000

    day 4 part 2 done...in php

--- day-4/part2.php
+++ day-4/part2.php
@@ -1,5 +1,6 @@
 <?php
 $file = fopen("day-4/input.txt", "r") or die("Unable to open file!");
+//$file = fopen("day-4/testcase.txt", "r") or die("Unable to open file!");
 $txt = fread($file,filesize("day-4/input.txt"));
 $txt = preg_split('/\s+/', $txt);
 $s = array();
@@ -17,16 +18,29 @@ $ylength = count($s);
 for ($y = 1; $y < $ylength - 1; $y+=1) {
 	for ($x = 1; $x < $xlength - 1; $x+=1) {
         if ($s[$y][$x] == 'A') { // look for center 'A'
-            if ($s[$y-1][$x-1] == 'M' && $s[$y-1][$y+1] == 'M') { // look for the m on top 
-                if ($s[$y+1][$x-1] == 'S' && $s[$y+1][$x+1] == 'S') { // look for the s on bottom 
+            $tl = $s[$y-1][$x-1]; // top left
+            $tr = $s[$y-1][$x+1]; // top right
+            $bl = $s[$y+1][$x-1]; // bottom left
+            $br = $s[$y+1][$x+1]; // bottom right
+            if ($tl == 'M' && $tr == 'M') { 
+                if ($bl == 'S' && $br == 'S') {
                     $amount += 1;
                 }
-            } else if ($s[$y-1][$x-1] == 'S' && $s[$y-1][$y+1] == 'S') { // look for the m on top 
-                if ($s[$y+1][$x-1] == 'M' && $s[$y+1][$x+1] == 'M') { // look for the s on bottom 
+            } else if ($tl == 'S' && $tr == 'S') {  
+                if ($bl == 'M' && $br == 'M') { 
+                    $amount += 1;
+                }
+            } else if ($tl == 'M' && $tr == 'S') {  
+                if ($bl == 'M' && $br == 'S') {  
+                    $amount += 1;
+                }
+            } else if ($tl == 'S' && $tr == 'M') {  
+                if ($bl == 'S' && $br == 'M') { 
                     $amount += 1;
                 }
             }
         }
     }
 }
+echo $amount;
 ?>
